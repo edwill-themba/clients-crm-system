3e8490a32ff92342e40a75e321fdc26d0a9d3246
edwill-themba <mpephothemba@gmail.com>
2023-11-26 08:46:57 -0600

    app setup and creation of users and clients tables

---
+++ app/Models/Client.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_number',
+        'date_of_birth',
+        'first_name',
+        'last_name',
+        'email',
+        'telephone',
+        'status'
+    ];
+    /**
+     * Sets the timestamps to false
+     */
+    public $timestamp = false;
+}
