f383ddc0a6da5d3cb2240f3f20a089a9304255d0
edwill-themba <mpephothemba@yahoo.com>
2023-12-01 10:59:45 -0600

    application is now complete

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,20 @@ import navbar from './components/navbar'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router/index'
 import store from './store/index'
+import {
+    library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+    fas
+} from '@fortawesome/free-solid-svg-icons'
+import {
+    FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
 
+library.add(fas)
 const app = createApp({})
 app.component('navbar', navbar)
+    .component('FontAwesomeIcon', FontAwesomeIcon)
     .use(bootstrap)
     .use(router)
     .use(store)
--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
+      <!-- menu -->
+      <div class="icon-menu">
+        <div  class="bars"  v-on:click="showNavbar">
+          <FontAwesomeIcon  icon="bars" class="icon-bars"/>
+        </div>
+        <div class="times" v-on:click="hideNavbar">
+          <FontAwesomeIcon  icon="times" class="icon-times" />
+        </div>
+      </div>
+      <!-- end menu -->
        <nav class="app-navbar">
          <div class="container">
             <router-link to="" class="brand">Themba-Client-CRM</router-link>
-            <ul class="menu">
+            <ul class="menu" v-on:click="hideNavbar">
               <li v-if="!isLoggedIn">
                 <router-link to="/">Admin Login</router-link>
               </li> 
@@ -17,8 +27,8 @@
                 <router-link to="/logout">Log Out</router-link>
               </li>
             </ul>
-         </div>
-       </nav>
+          </div>
+        </nav>
        <router-view></router-view>
     </div>
 </template>
@@ -33,11 +43,32 @@ export default {
     isCurrentUser() {
       return this.$store.getters.isCurrentUser;
     }
+  },
+  methods: {
+    showNavbar() {
+      const bar = document.querySelector(".bars");
+      const times = document.querySelector(".times");
+      const menu = document.querySelector(".menu");
+      bar.classList.toggle("inactive");
+      times.classList.add("active");
+      menu.classList.toggle("active");
+    },
+    hideNavbar() {
+      const bar = document.querySelector(".bars");
+      const times = document.querySelector(".times");
+      const menu = document.querySelector(".menu");
+      bar.classList.remove("inactive");
+      times.classList.remove("active");
+      menu.classList.remove("active");
+    }
   }
 };
 </script>
 
 <style scoped>
+.app-navbar {
+  position: relative;
+}
 .app-navbar .container {
   display: flex;
   justify-content: space-between;
@@ -64,5 +95,63 @@ export default {
 }
 .app-navbar .container .menu li a:hover {
   color: #f1ba50;
+}
+.icon-menu {
+  position: absolute;
+  margin-left: 90%;
+  margin-top: 5%;
+}
+.icon-menu .icon-bars {
+  color: #f1ba50;
+  display: none;
+}
+.icon-menu .icon-times {
+  color: #04abc1;
+  display: none;
+}
+
+@media (max-width: 798px) {
+  .brand {
+    font-size: 16px;
+    margin-left: 20px;
+  }
+  .menu {
+    margin-top: 60px;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    transform: translateX(-100%);
+    background: #000000;
+    bottom: 0;
+    top: 60;
+    position: fixed;
+    display: block;
+    text-align: center;
+  }
+  .menu.active {
+    left: 0;
+    transform: translateX(0px);
+  }
+  .menu li {
+    margin-top: 10px;
+  }
+
+  .icon-menu {
+    display: inline;
+    z-index: 100;
+  }
+  .bars .icon-bars {
+    display: inline-block;
+    right: 10%;
+  }
+
+  .bars.inactive .icon-bars {
+    display: none;
+  }
+  .times.active .icon-times {
+    display: inline-block;
+  }
 }
 </style>
